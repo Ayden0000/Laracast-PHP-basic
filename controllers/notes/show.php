e0a3f676a48eacb57a721815c78ae1be6fc200af
Ayden0000 <azi.nyein@gmail.com>
2023-09-12 15:07:56 +0630

    delete button added

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -8,11 +8,25 @@ $db = new Database($config['database']);
 $id = $_GET['id'];
 $currentId = 1;
 
-$note = $db->query('SELECT * FROM `posts` WHERE id= :id', ['id' => $id])->findOrFail();
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $note = $db->query('SELECT * FROM `posts` WHERE id= :id', [
+        'id' => $id ])->findOrFail();
 
-authorize($note['user_id'] === $currentId);
+    authorize($note['user_id'] === $currentId);
 
-view('/notes/show.view.php', [
-    'heading' => "Note",
-    'note' => $note
-]);
+    $db->query('DELETE  FROM `posts` WHERE id = :id', [
+        'id' => $id
+    ]);
+    header('location: /notes');
+    exit();
+}else {
+    $note = $db->query('SELECT * FROM `posts` WHERE id= :id', ['id' => $id])->findOrFail();
+
+    authorize($note['user_id'] === $currentId);
+
+    view('/notes/show.view.php', [
+        'heading' => "Note",
+        'note' => $note
+    ]);
+}
+
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -5,13 +5,19 @@
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <h1 class="text-2xl font-bold"><?= $note['title'] ?></h1>
-        <div class="block w-full mt-6 border-0 py-2 pl-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300">
-            <p class="text-m"><?= $note['description'] ?></p>
-        </div>
         <p class="text-blue-500 underline mt-4">
             <a href="/notes" >Go Back...</a>
         </p>
+        <div class="mt-5">
+            <h1 class="text-2xl font-bold"><?= $note['title'] ?></h1>
+            <div class="block w-full mt-6 border-0 py-2 pl-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300">
+                <p class="text-m"><?= $note['description'] ?></p>
+            </div>
+        </div>
+        <form class="mt-5" method="POST">
+            <input type="hidden" name="id" value="<?= $note['id'] ?>">
+            <button type="submit" class="text-red-500">Delete</button>
+        </form>
     </div>
 </main>
 
